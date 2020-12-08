f5b8cffa172ea173d58f6797f62a76fa3646d211
Fernando Santos <fernandosantos511@yahoo.com.br>
2020-12-08 19:38:07 -0300

    feat: compenet menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-<<<<<<< HEAD
 import Menu from "./components/menu";
 import MainRoutes from "./router";
 
@@ -9,31 +7,6 @@ const App = () => {
       <Menu />
       <MainRoutes />
     </>
-=======
-import Router from "./router";
-import { MainPageLayout } from "./styles";
-import logo from "./images/kenzie_logo.png";
-import ProjectDetails from "./components/projectDetails";
-import AboutUs from "./components/aboutUs";
-
-const App = () => {
-  return (
-    <MainPageLayout>
-      <header>
-        <Link to="/login">Login </Link>
-        <Link to="/user-registration">Cadastro</Link>
-        <Link to="/users-list">Usuários cadastrados</Link>
-      </header>
-      <h1>KenzieHub</h1>
-      <br />
-      <img src={logo} alt="Kenzie Logo" width="100px" height="100px" />
-      <Router />
-      <footer>
-        <ProjectDetails />
-        <AboutUs />
-      </footer>
-    </MainPageLayout>
->>>>>>> 172f3ee3cc2e7b5a169737d083fca54721d9caa4
   );
 };
 
