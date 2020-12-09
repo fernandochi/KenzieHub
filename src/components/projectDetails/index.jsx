db2c2552853ee793dd45148a4770b5f01235e01c
Fernando Santos <fernandosantos511@yahoo.com.br>
2020-12-09 17:49:30 +0000

    feat: home-page functionality, aboutUs and projectDetails components

--- src/components/projectDetails/index.jsx
+++ src/components/projectDetails/index.jsx
@@ -1,15 +1,29 @@
-import { ProjectDetailsDiv } from "./styles";
+import { Modal, Button } from "antd";
+import { useState } from "react";
 
 const ProjectDetails = () => {
+  const [visible, setVisible] = useState(false);
   return (
-    <ProjectDetailsDiv>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, quasi.
-        A, enim accusamus! Reprehenderit ullam beatae facilis excepturi
-        molestias et autem, commodi, molestiae eius qui hic quasi! Eius,
-        doloribus voluptate.
-      </p>
-    </ProjectDetailsDiv>
+    <>
+      <Button type="primary" onClick={() => setVisible(true)}>
+        Sobre o projeto
+      </Button>
+      <Modal
+        title="Integrantes do Projeto"
+        centered
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        width={1000}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, quasi.
+          A, enim accusamus! Reprehenderit ullam beatae facilis excepturi
+          molestias et autem, commodi, molestiae eius qui hic quasi! Eius,
+          doloribus voluptate.
+        </p>
+      </Modal>
+    </>
   );
 };
 
