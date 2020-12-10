1a9e677a7eb2b55c5537db0d7650f36102556a0a
Amanda Gonçalves <amandapergon@gmail.com>
2020-12-10 12:50:56 -0300

    fix: authorization token

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,16 +1,43 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Switch, Route, useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 import Profile from "../pages/profile";
 import Login from "../pages/login";
 import Technologies from "../pages/technologies";
 import UserRegistration from "../pages/userRegistration";
-import Portifolio from "../pages/portfolio";
 import Users from "../pages/users";
 import UsersList from "../pages/usersList";
 import HomePage from "../pages/homePage";
+import Portfolio from "../pages/portfolio";
 
 const MainRoutes = () => {
+  const location = useLocation();
+  const history = useHistory();
+  const [isAuthorized, setIsAuthorized] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios
+        .get("https://kenziehub.me/profile", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((data) => {
+          console.log(data);
+          setIsAuthorized(true);
+        })
+        .catch(() => {
+          setIsAuthorized(false);
+          localStorage.clear();
+        });
+    } else {
+      localStorage.clear();
+      setIsAuthorized(false);
+    }
+  }, [location.pathname]);
+
   return (
     <Switch>
       <Route exact path="/">
@@ -19,24 +46,53 @@ const MainRoutes = () => {
       <Route path="/login">
         <Login />
       </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-      <Route path="/portifolio">
-        <Portifolio />
-      </Route>
-      <Route path="/technologies">
-        <Technologies />
-      </Route>
       <Route path="/user-registration">
         <UserRegistration />
-      </Route>
-      <Route exact path="/users">
-        <Users />
       </Route>
       <Route exact path="/users-list">
         <UsersList />
       </Route>
+      <Route
+        exact
+        path="/users"
+        render={(props) =>
+          isAuthorized ? (
+            <Users {...props} />
+          ) : (
+            history.push("/user-registration")
+          )
+        }
+      />
+      <Route
+        path="/profile"
+        render={(props) =>
+          isAuthorized ? (
+            <Profile {...props} />
+          ) : (
+            history.push("/user-registration")
+          )
+        }
+      />
+      <Route
+        path="/portfolio"
+        render={(props) =>
+          isAuthorized ? (
+            <Portfolio {...props} />
+          ) : (
+            history.push("user-registration")
+          )
+        }
+      />
+      <Route
+        path="/technologies"
+        render={(props) =>
+          isAuthorized ? (
+            <Technologies {...props} />
+          ) : (
+            history.push("user-registrarion")
+          )
+        }
+      />
     </Switch>
   );
 };
